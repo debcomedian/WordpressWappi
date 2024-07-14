2a434def014047a3e8a27d49b33e56f82c7c97e3
Vitaliy <144813657+debcomedian@users.noreply.github.com>
2024-07-14 09:52:00 +0300

    'done'

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# WordpressWappi
-Wappi module for sending messages to WhatsApp and Telegram when placing or processing an order
+# Wappi: Whatsapp and Telegram Notifications for WooCommerce
+
+**Contributors:** support@wappi.pro  
+**Tags:** woo commerce, woocommerce, ecommerce, whatsapp, telegram, notification, whatsapp notification, telegram notification, уведомления  
+**Requires at least:** 3.8  
+**Tested up to:** 6.5  
+**Stable tag:** 1.0  
+**Requires PHP:** 7.4  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html  
+
+Automatic Whatsapp and Telegram notifications about order statuses for WooCommerce store administrators and customers.
+
+## Description
+
+With the "Wappi: Whatsapp and Telegram Notifications for WooCommerce" plugin, you can send automatic Whatsapp and Telegram notifications about order statuses to administrators and customers of a WordPress WooCommerce online store.
+
+Note: This is NOT a Whatsapp Business API (WABA), but a connection to your own Whatsapp account via QR code scanning.
+
+Features:
+
+* Supports the Wordpress WooCommerce module.
+* Send notifications from your own Whatsapp account. Connect your personal Whatsapp account by scanning a QR code and send automatic messages from it.
+* Send notifications from your own Telegram account. Connect your personal Telegram account (not a bot) and send automatic messages from it.
+* Automatic notifications to administrators. Send notifications to the administrator or seller about new orders and order status changes. You can specify multiple custom numbers.
+* Automatic notifications to customers. Send notifications to customers about order confirmation and order status changes.
+* Set individual notification templates with variables. For each notification, you can set your own text using variables: products, quantity, price, name, phone, address, custom fields (tracking number).
+
+[WEBSITE](https://dev.wappi.pro/) | [SUPPORT](https://t.me/wappi_support)
+
+## Installation
+
+1. Make sure you have the latest version of the [WooCommerce](http://www.woothemes.com/woocommerce) plugin installed.
+2. There are several ways to install the plugin:
+    * Through the plugin directory:
+        - In the admin panel, go to the *Plugins* page and click *Add New*.
+        - Find the Wappi: Whatsapp and Telegram Notifications for WooCommerce plugin.
+        - Click the *Install* button.
+    * Through the console:
+        - Download the plugin here: https://dev.wappi.pro/integrations/wordpress.
+        - In the admin panel, go to the *Plugins* page and click *Add New*.
+        - Go to the *Upload* tab, click *Browse*, and select the plugin archive. Click *Install*.
+    * By FTP:
+        - Download the plugin here: https://dev.wappi.pro/integrations/wordpress.
+        - Unpack the archive and upload the contents via FTP to the your-domain/wp-content/plugins folder.
+        - In the admin panel, go to the *Plugins* page and click *Install* next to the newly appeared plugin.
+3. Once the plugin is installed, click *Activate Plugin*.
+4. Hover over the *WooCommerce* menu item and select *Wappi*.
+5. In the settings, enter the API Token and profile ID (found at https://dev.wappi.pro/dashboard), as well as the seller's Whatsapp number.
+6. If necessary, specify the statuses for each type of notification and the text.
+7. Click the Save button.
+
+## Screenshots
+
+1. Wappi plugin settings
+
+## Changelog
+
+### 1.0
+
+- First version
---
+++ wappi_send_message_woocommerce/wappi_send_message_woocommerse.php
@@ -0,0 +1,495 @@
+<?php
+/*
+Plugin Name: Wappi
+Plugin URI:
+Description: Whatsapp и Telegram уведомления о заказах WooCommerce через Wappi
+Version: 1.0
+Author: Wappi
+Author URI: https://wappi.pro
+*/
+if (!is_callable('is_plugin_active')) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+if (is_plugin_active('woocommerce/woocommerce.php')) {
+	add_action('plugins_loaded', 'wappi_woocommerce::load');
+}
+
+register_activation_hook( __FILE__, 'wappi_woocommerce::activate' );
+
+function wappi_send( $phone, $message )
+{
+    return (new wappi_woocommerce())->send( $phone, $message );
+}
+
+class wappi_woocommerce {
+
+	public static function load() {
+		$_this = new self();
+		add_action( 'admin_menu', array($_this,'admin_menu'));
+		add_action( 'woocommerce_new_order', array($_this,'status_changed'),  10, 1  );
+		add_action( 'woocommerce_order_status_changed', array($_this, 'status_changed'), 10, 3);
+		return $_this;
+	}
+
+	public function admin_menu() {
+		add_submenu_page('woocommerce', 'Whatsapp и Telegram уведомления через Wappi', 'Wappi', 'manage_woocommerce', 'wappi_settings', array(&$this,'options'));
+	}
+
+	public static function activate()
+	{
+		register_uninstall_hook( __FILE__, 'wappi_woocommerce::uninstall' );
+	}
+
+	public static function uninstall() {
+	    delete_option('wappi_apikey');
+		delete_option('wappi_platform');
+	    delete_option('wappi_sender');
+	    delete_option('wappi_vendor_phone');
+		delete_option('wappi_vendor_status1');
+	    delete_option('wappi_vendor_msg1');
+	    delete_option('wappi_vendor_status2');
+		delete_option('wappi_vendor_msg2');
+		delete_option('wappi_shopper_status1');
+		delete_option('wappi_shopper_msg1');
+		delete_option('wappi_shopper_status2');
+		delete_option('wappi_shopper_msg2');
+	}
+
+	private function _get_parameters()
+	{
+		return array(
+			'apikey' => get_option('wappi_apikey'),
+			'platform' => get_option('wappi_platform'),
+			'sender' => get_option('wappi_sender'),
+			'vendor_phone' => get_option('wappi_vendor_phone'),
+			'vendor_status1' => get_option('wappi_vendor_status1','processing'),
+			'vendor_msg1' => get_option('wappi_vendor_msg1', 'Поступил заказ на сумму {SUM}. Номер заказа {NUM}'),
+			'vendor_status2' => get_option('wappi_vendor_status2','cancelled,failed'),
+			'vendor_msg2' => get_option('wappi_vendor_msg2','Статус заказа изменился на {NEW_STATUS}. Номер заказа {NUM}'),
+			'shopper_status1' => get_option('wappi_shopper_status1', 'processing'),
+			'shopper_msg1' => get_option('wappi_shopper_msg1','Ваш заказ на сумму {SUM} принят. Номер заказа {NUM}'),
+			'shopper_status2' => get_option('wappi_shopper_status2','completed'),
+			'shopper_msg2' => get_option('wappi_shopper_msg2','Статус вашего заказа изменился на {NEW_STATUS}. Номер заказа {NUM}'),
+			'shopper_status3' => get_option('wappi_shopper_status3',''),
+			'shopper_msg3' => get_option('wappi_shopper_msg3','')
+		);
+	}
+
+	public function options() {
+
+		$p = $this->_get_parameters();
+		$message = '';
+		if (isset($_POST['wappi_settings_nonce_field']) && check_admin_referer('wappi_settings_nonce_action', 'wappi_settings_nonce_field')) {
+			if ( isset($_POST['apikey']) ) {
+				foreach( $p as $k => $vv ) {
+					$v = '';
+					if (isset($_POST[$k])) {
+						if ( is_string($_POST[$k]) ) {
+							$v = sanitize_text_field( $_POST[ $k ] );
+						} else if ( is_array($_POST[$k]) ) {
+							$v = sanitize_text_field( implode(',', $_POST[$k]) );
+						}
+					}
+					update_option('wappi_' . $k, $v);
+				}
+
+				if (!isset($_POST['test']) ) {
+					wp_redirect(admin_url('admin.php?page=wappi_settings&status=updated'));
+					return;
+				}
+				$p = $this->_get_parameters();
+			}
+		}
+
+		/* pending Order received (unpaid)
+		failed – Payment failed or was declined (unpaid). Note that this status may not show immediately and instead show as Pending until verified (i.e., PayPal)
+		processing – Payment received and stock has been reduced – the order is awaiting fulfillment. All product orders require processing, except those that are Digital and Downloadable.
+		completed – Order fulfilled and complete – requires no further action
+		on-hold – Awaiting payment – stock is reduced, but you need to confirm payment
+		cancelled – Cancelled by an admin or the customer – no further action required
+		refunded – Refunded by an admin – no further action required */
+
+		$msg = array(
+			array( 'Оповещение продавцу о новом заказе', 'vendor_status1', 'vendor_msg1' ),
+			array( 'Оповещение продавцу о смене статуса', 'vendor_status2', 'vendor_msg2' ),
+			array( 'Оповещение покупателю о подтверждении заказа', 'shopper_status1', 'shopper_msg1' ),
+			array( 'Оповещение покупателю о смене статуса', 'shopper_status2', 'shopper_msg2' ),
+			array( 'Оповещение покупателю о смене статуса (дополнительно)', 'shopper_status3', 'shopper_msg3' )
+		);
+?>
+		<html>
+		<head>
+			<style>
+			li {
+				font-size: 1.4em;
+				padding-bottom: 5px;
+			}
+			</style>
+		</head>
+		<body>
+			<div class="wrap woocommerce">
+
+				<form method="post" id="mainform" action="<?php echo esc_attr(admin_url('admin.php?page=wappi_settings')) ?>">
+					<?php wp_nonce_field('wappi_settings_nonce_action', 'wappi_settings_nonce_field'); ?>
+					<h2>Whatsapp или Telegram оповещения о заказах через Wappi</h2>
+					<img src="/../wp-content/plugins/wappi/images/logo.webp" alt="А где лого? (^._.^)~" style="max-width: 130px; margin-left: 10px;">
+					<h3>Как пользоваться</h3>
+					<ol>
+						<li>Перейдите на <a href="https://wappi.pro/">wappi.pro</a> и зарегистрируйтесь</li>
+						<li>Добавьте необходимый профиль и авторизуйте ваш номер, с которого будут отправляться оповещения</li>
+						<li>В личном кабинете посмотрите токен API и ID профиля и возвращайтесь к настройкам</li>
+						<li>Заполните необходимые поля и нажмите Сохранить</li>
+						<li>Если возникли проблемы - воспользуйтесь <a href="https://wappi.pro/integrations/wordpress">инструкцией</a></li>
+					</ol>
+
+					<?php echo (isset($_GET['status']) && $_GET['status'] === 'updated' ) ? '<p style="color: green">Настройки обновлены</p>' : '' ?>
+					<?php echo ( $last_e = get_option('wappi_last_error')) ? '<p>Последняя ошибка:<br/>'.esc_html( $last_e ).'</p>' : '' ?>
+					<table class="form-table">
+						<tr><th><label for="apikey">Токен API</label></th><td><input title="40-символьный ключ доступа к Wappi" 
+						required name="apikey" pattern=".{40}" id="apikey" value="<?php echo esc_attr( $p['apikey'] ) ?>" size="43"/><br/>
+						<small>Укажите ваш токен, найти можно <a href="https://wappi.pro/dashboard" target="_blank">здесь</a></small></td></tr>
+						<tr><th><label>Статус приложения:</label></th><td><?php
+								if ( $p['apikey']) {
+									$this->_output_send_status($p, $message);
+								} else
+									echo '<span style="color: red">Нужно ввести API-ключ</span>';
+								?></td></tr>
+						<tr><th><label for="sender">ID профиля</label></th><td><input required name="sender" id="sender" value="<?php echo esc_attr( $p['sender'] ) ?>" /><br/>
+						<small>Узнать свой ID можно в личном кабинете Wappi</td></tr>
+						<tr><th><label for="vendor_phone">Номер продавца</label></th><td><input title="79008007060, 79008007060" required name="vendor_phone" pattern="^\d{11}(, \d{11})*$" id="vendor_phone" value="<?php echo esc_attr( $p['vendor_phone'] )  ?>" size="50"/>
+						<br/><small>Например, 79991112233, можно указать несколько через запятую.</small></td></tr>
+					</table>
+					<input type="submit" class="button-secondary" name="test" value="Отправить тестовое сообщение продавцу" />
+					<table class="form-table">
+						<?php foreach( $msg as $m) { ?>
+						<tr><th><label for="<?php echo esc_attr( $m[2] ) ?>"><?php echo esc_html( $m[0] ) ?></label></th><td>
+						<?php 
+						$status_text = sprintf('Статус: %s', $this->_init_checkboxes($m[1], $p[$m[1]]));
+						$allowed_html = array(
+							'label' => array(),
+							'input' => array(
+								'type' => array(),
+								'name' => array(),
+								'value' => array(),
+								'checked' => array()
+							),
+						);
+						echo esc_html('Статус: ') . wp_kses($status_text, $allowed_html);
+						?><br/>
+						Текст: <input name="<?php echo esc_attr( $m[2] ) ?>" id="<?php echo esc_attr( $m[2] ) ?>" value="<?php echo esc_attr( $p[ $m[2] ] )  ?>" size="80" />
+						</td></tr>
+
+							<?php }	?>
+						
+						<tr><th><label>Можно вставить переменные</label></th><td>
+							<pre><code>{NUM} - номер заказа, {FNUM} - №номерзаказа, {SUM} - сумма заказа, {FSUM} - суммазаказа руб., {EMAIL} - эл.почта покупателя,
+	{PHONE} - телефон покупателя, {FIRSTNAME} - имя покупателя, {LASTNAME} - фамилия покупателя,
+	{CITY} - город доставки, {ADDRESS} - адрес доставки, {BLOGNAME} - название блога/магазина,
+	{OLD_STATUS} - старый статус, {NEW_STATUS} - новый статус, {ITEMS} - список заказанных товаров
+	{COMMENT} - комментарий покупателя к заказу
+	<strong>{Произвольное поле}</strong> - вставка значения произвольного поля, которое вы или плагины добавили к заказу, 
+	например, {post_tracking_number} или {ems_tracking_number} если установлен плагин 
+	. Чувствительно к регистру символов!</code></pre></td></tr>
+					</table>
+					<input type="submit" class="button-primary" value="Сохранить">
+				</form>
+			</div>
+		</body>
+		</html>
+<?php
+	}
+
+	private function _output_send_status ($p, $message) {
+		if (isset($_POST['wappi_settings_nonce_field']) && check_admin_referer('wappi_settings_nonce_action', 'wappi_settings_nonce_field')) {
+			if ( isset($_POST['test'])) {
+				$data = array(
+					'%s' => '1234.56',
+					'%n' => '7890',
+					'{SUM}' => '1234.56',
+					'{FSUM}' => '1234.56 руб.',
+					'{NUM}' => '7890',
+					'{ITEMS}' => 'Название товара: 2x150=300',
+					'{EMAIL}' => 'pokupatel@mail.ru',
+					'{PHONE}' => '+79000000000',
+					'{FIRSTNAME}' => 'Сергей',
+					'{LASTNAME}' => 'Смирнов',
+					'{CITY}' => 'г. Омск',
+					'{ADDRESS}' => 'ул. Ленина, д. 1, кв. 2',
+					'{BLOGNAME}' => get_bloginfo('name'),
+					'{OLD_STATUS}' => 'Обработка',
+					'{NEW_STATUS}' => 'Выполнен',
+					'{COMMENT}' => 'Код домофона 123, после обеда',
+					'{' => '*',
+					'}' => '*',
+				);
+				$vendor_phone = sanitize_text_field( $_POST['vendor_phone'] );
+				$message = str_replace( array_keys($data), array_values($data), sanitize_text_field( $_POST['vendor_msg1'] ) );
+			}
+			$data = $this->_get_profile_info();	
+			if (sizeof($data) > 2) {
+				$time_sub = new DateTime($data['payment_expired_at']);
+				$time_curr = new DateTime;
+				if ($time_sub < $time_curr)
+					echo '<span style="color: red">Сообщение не отправлено</span>&nbsp;&nbsp;|&nbsp;&nbsp;Профиль не оплачен. <a href="https://wappi.pro/dashboard" target="_blank">Оплатите на сайте </a>';
+				else {
+					update_option('wappi_platform', $data['platform'] === 'tg'? 't' : '');
+					$this->send($p['vendor_phone'], $message);
+					$this->_save_info();
+					$time_diff = $time_curr->diff($time_sub);
+					$days_diff = $time_diff->days;
+					$hours_diff = $time_diff->h;
+					if (isset($_POST['test']))
+						echo '<span style="color: green">Тестовое сообщение успешно отправлено продавцу';
+					else 
+						echo '<span style="color: green">Сообщение успешно отправлено';
+					echo '</span>&nbsp;&nbsp;|&nbsp;&nbsp<span>' . esc_html('Профиль оплачен до: ' . $time_sub->format('Y-m-d') .', Подписка истекает через: ');
+					$days_diff_last_num = $days_diff % 10;
+					$hours_diff_last_num = $hours_diff % 10;
+
+					if ($days_diff !== 0) {
+						echo esc_html($days_diff);
+						if ($days_diff_last_num > 4 || ($days_diff > 10 && $days_diff < 21))
+							echo ' дней ';
+						else if ($days_diff_last_num === 1 )
+							echo ' день ';
+						else
+							echo ' дня ';
+					}
+					echo esc_html($hours_diff);
+					if ($hours_diff_last_num > 4 || ($hours_diff > 10 && $hours_diff < 20) || $hours_diff_last_num === 0) 
+						echo ' часов';	
+					else if ($hours_diff_last_num === 1)
+						echo ' час';
+					else 
+						echo ' часа';
+					echo '</span>';	
+				}		
+			} else 
+				echo '<span style="color: red">Ошибка отправки сообщения, скорее всего вы ввели неверный токен API или ID профиля</span>';
+		}
+	}
+
+	public function status_changed($order_id, $old_status = 'pending', $new_status = 'pending')
+	{
+		$p = $this->_get_parameters();
+
+		if ( $p['apikey'] ) {
+
+			$o = new WC_Order($order_id);
+
+			// SMS to sender
+			if ( strpos($p['vendor_status1'], $new_status) !== false ) // new order
+				$this->_send( $p['vendor_phone'], $p['vendor_msg1'], $o, $old_status, $new_status );
+			else if ( strpos($p['vendor_status2'], $new_status) !== false ) // new status
+				$this->_send( $p['vendor_phone'], $p['vendor_msg2'], $o, $old_status, $new_status );
+			// SMS to shopper
+			if ( strpos($p['shopper_status1'], $new_status) !== false ) // confirmed order
+				$this->_send( $o->get_billing_phone(), $p['shopper_msg1'], $o, $old_status, $new_status );
+			else if ( strpos($p['shopper_status2'], $new_status) !== false ) // new status
+				$this->_send( $o->get_billing_phone(), $p['shopper_msg2'], $o, $old_status, $new_status );
+			else if ( strpos($p['shopper_status3'], $new_status) !== false ) // new status alt
+				$this->_send( $o->get_billing_phone(), $p['shopper_msg3'], $o, $old_status, $new_status );
+		}
+	}
+
+	private function _init_checkboxes( $name, $selected )
+	{  
+		$selected = explode(',',$selected);
+
+		$r = '';
+		foreach( wc_get_order_statuses() as $k => $v ) {
+			$k = substr( $k, 3 );
+			$r .= '<label><input type="checkbox" name="'.$name.'[]"'.( in_array( $k, $selected, true ) ? ' checked="checked"' : '').' value="'.$k.'" /> '.$v.'</label>&nbsp;&nbsp;';
+		}
+		return $r;
+	}
+
+		/**
+	 * @param $phone
+	 * @param $message
+	 * @param $order WC_Order
+	 * @param $old_status
+	 * @param $new_status
+	 *
+	 * @return void
+	 */
+	
+	private function _send($phone, $message, $order, $old_status, $new_status ) 
+	{
+		//file_put_contents( __FILE__.'.log', print_r( $order, true) );
+		$search  = array(
+			'{NUM}',
+			'{FNUM}',
+			'{SUM}',
+			'{FSUM}',
+			'{EMAIL}',
+			'{PHONE}',
+			'{FIRSTNAME}',
+			'{LASTNAME}',
+			'{CITY}',
+			'{ADDRESS}',
+			'{BLOGNAME}',
+			'{OLD_STATUS}',
+			'{NEW_STATUS}',
+			'{COMMENT}'
+		);
+
+		$replace = array(
+			$order->get_order_number(),
+			'№' . $order->get_order_number(),
+			$order->get_total(),
+			wp_strip_all_tags( $order->get_formatted_order_total( false, false ) ),
+			$order->get_billing_email(),
+			$order->get_billing_phone(),
+			($s = $order->get_shipping_first_name()) ? $s : $order->get_billing_first_name(),
+			($s = $order->get_shipping_last_name()) ? $s : $order->get_billing_last_name(),
+			($s = $order->get_shipping_city()) ? $s : $order->get_shipping_city(),
+			trim(
+				(($s = $order->get_shipping_address_1()) ? $s : $order->get_billing_address_1())
+				.' '
+				.(($s = $order->get_shipping_address_2()) ? $s : $order->get_billing_address_2())
+			),
+			get_option( 'blogname' ),
+			wc_get_order_status_name( $old_status ),
+			wc_get_order_status_name( $new_status ),
+			$order->get_customer_note()
+		);
+
+		if ( strpos( $message, '{ITEMS}' ) !== false ) {
+			$items     = $order->get_items();
+			$items_str = '';
+			foreach ( $items as $i ) {
+				/* @var $i WC_Order_Item_Product */
+				$name = $i['name'];
+				if ( ( $_p = $i->get_product() ) && ( $sku = $_p->get_sku() ) ) {
+					$name = $sku . ' ' . $name;
+				}
+				$items_str .= "\n" . $name . ': ' . $i['qty'] . 'x' . $order->get_item_total( $i ) . '=' . $order->get_line_total( $i );
+			}
+			$sh = $order->get_shipping_methods();
+			foreach ( $sh as $i ) {
+				$items_str .= "\n" . __( 'Shipping', 'woocommerce' ) . ': ' . $i['name'] . '=' . $i['cost'];
+			}
+			$items_str .= "\n";
+			$search[]  = '{ITEMS}';
+			$replace[] = wp_strip_all_tags( $items_str );
+		}
+
+		if ( $meta = get_post_meta( $order->get_id() ) ) {
+			foreach( $meta as $k => $v ) {
+				$search[] = '{'.$k.'}';
+				$replace[] = $v[0];
+			}
+		}
+
+		foreach ( $replace as $k => $v ) {
+			$replace[ $k ] = html_entity_decode( $v );
+		}
+		$message = str_replace( $search, $replace, $message );
+		$message = preg_replace('/\s?\{[^}]+\}/','', $message ); // remove unknown {VAR}
+		$message = trim( $message );
+		$message = mb_substr( $message, 0, 670 );
+		$this->send( $phone, $message );
+	}
+
+	public function send( $phone, $message ) {
+		$profile_id = get_option('wappi_sender');
+		$this->_post($phone, $message, $profile_id);
+	}
+
+	private function _get_profile_info() {
+		$profile_id = get_option('wappi_sender');
+		$apikey = get_option('wappi_apikey');
+		$url = 'https://wappi.pro/api/sync/get/status?profile_id=' . urlencode($profile_id);
+
+		$args = array(
+			'method' => 'GET',
+			'headers' => array(
+				'Accept' => 'application/json',
+				'Authorization' => $apikey,
+				'Content-Type' => 'application/json',
+			),
+		);
+
+		$response = wp_remote_get($url, $args);
+		if (is_wp_error($response)) {
+			echo '<span style="color: red">' . esc_html('Ошибка HTTP: ' . $response->get_error_message()) . '</span>';
+			return "";
+		} else {
+			$body = wp_remote_retrieve_body($response);
+			$data = json_decode($body, true);
+			// Проверка на ошибки JSON
+			if (!(json_last_error() === JSON_ERROR_NONE)) {
+				echo '<span style="color: red">' . esc_html('Ошибка JSON: ' . json_last_error_msg()) . '</span>';
+			}
+		}
+		return $data;		
+	}
+
+	private function _save_info() {
+		$apikey = get_option('wappi_apikey');
+		$profile_id = get_option('wappi_sender');
+		$message_json = json_encode(array(
+			'url' => $_SERVER['HTTP_REFERER'],
+			'module' => 'wp',
+			'profile_uuid' => $profile_id,
+		));
+
+		$url = 'https://dev.wappi.pro/tapi/addInstall?profile_id=' . urlencode($profile_id);
+
+		$args = array(
+			'body' => $message_json,
+			'headers' => array(
+				'Accept' => 'application/json',
+				'Authorization' => $apikey,
+				'Content-Type' => 'application/json',
+			),
+			'method' => 'POST',
+			'data_format' => 'body',
+		);
+
+		$response = wp_remote_post($url, $args);
+
+		if (is_wp_error($response)) {
+			echo '<span style="color: red">' . esc_html('Error: ' . $response->get_error_message()) . '</span>';
+		} else if (!(json_last_error() === JSON_ERROR_NONE)) {
+			echo '<span style="color: red">' . esc_html('Ошибка JSON: ' . json_last_error_msg()) . '</span>';
+		}
+	}
+	private function _post($phone, $message, $profile_id) 
+	{
+		$platform = get_option('wappi_platform');
+		$apikey = get_option('wappi_apikey');
+		$phone_array = explode(', ',$phone);
+		
+		foreach ($phone_array as $phone) {
+			$message_json = json_encode(array(
+				'recipient' => $phone,
+				'body' => $message
+			));
+		
+			$url = 'https://wappi.pro/' . $platform . 'api/sync/message/send?profile_id=' . urlencode($profile_id);
+		
+			$args = array(
+				'body' => $message_json,
+				'headers' => array(
+					'Accept' => 'application/json',
+					'Authorization' => $apikey,
+					'Content-Type' => 'application/json',
+				),
+				'method' => 'POST',
+				'data_format' => 'body',
+			);
+		
+			$response = wp_remote_post($url, $args);
+		
+			if (is_wp_error($response)) {
+				echo '<span style="color: red">' . esc_html('Error: ' . $response->get_error_message()) . '</span>';
+				break;
+			} else if (json_last_error() !== JSON_ERROR_NONE) {
+				echo '<span style="color: red">' . esc_html('Ошибка JSON: ' . json_last_error_msg()) . '</span>';
+				break;
+			}
+		}
+	}
+}
